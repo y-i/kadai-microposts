6c55f84c41bffa14c0f1219796178e7e4ae5fea5
y-i <y-i@users.noreply.github.com>
2017-04-19 00:18:37 +0000

    fix micropost model
    add :dependent to delete favorite posts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
+  has_many :favorites, :dependent => :destroy
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
 end
